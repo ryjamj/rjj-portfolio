ed478c8ef40152afb0b4bd10b71e4112c21677df
ryjamj <ryjamesj@gmail.com>
2024-04-12 16:32:37 -0600

    added email service to contact me

--- src/components/AppStructure/ContactMe/ContactMe.tsx
+++ src/components/AppStructure/ContactMe/ContactMe.tsx
@@ -1,25 +1,46 @@
-// import React from "react";
 import { Box, TextField, Typography, Grid, Button } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import emailjs from "emailjs-com";
+import { useState, useRef } from "react";
 import "./ContactMe.scss";
 
 export default function ContactMe() {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [subject, setSubject] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
+  const form = useRef<HTMLFormElement>(null);
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_xicbqns", "template_pz87z9c", form.current!, "MdqOAAwT-x0lz6_-q").then(
+      () => {
+        console.log("SUCCESS!");
+      },
+      (error) => {
+        console.log("FAILED...", error.text);
+      }
+    );
+  };
+
   return (
     <Box id="contact-id" className="contact-section">
       <h2>Contact me...</h2>
       <Grid className="contact-form">
-        <Box className="contact-details">
-          <TextField fullWidth={true} label="Name" placeholder="Your name" />
-          <TextField fullWidth={true} label="Email" placeholder="Your email address" />
-          <TextField fullWidth={true} label="Subject" placeholder="Reason for your message" />
-        </Box>
-        <Box className="contact-message">
-          <TextField fullWidth={true} label="Message" variant="outlined" multiline rows={4} />
-          <Button fullWidth={true} type="submit" endIcon={<SendIcon />} className="contact-me-button">
-            Send
-          </Button>
-          {/* <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button> */}
-        </Box>
+        <form ref={form} onSubmit={sendEmail}>
+          <Box className="contact-details">
+            <TextField fullWidth value={name} type="text" onChange={(e) => setName(e.target.value)} label="Name" placeholder="Your name" />
+            <TextField fullWidth value={email} type="email" onChange={(e) => setEmail(e.target.value)} label="Email" placeholder="Your email address" />
+            <TextField fullWidth value={subject} type="text" onChange={(e) => setSubject(e.target.value)} label="Subject" placeholder="Reason for your message" />
+          </Box>
+          <Box className="contact-message">
+            <TextField fullWidth value={message} onChange={(e) => setMessage(e.target.value)} label="Message" variant="outlined" multiline rows={4} />
+            <Button fullWidth type="submit" endIcon={<SendIcon />} className="contact-me-button">
+              Send
+            </Button>
+          </Box>
+        </form>
       </Grid>
     </Box>
   );
