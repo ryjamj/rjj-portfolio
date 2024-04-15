0b3001e1865657d76b185211e72f62bc98666edf
ryjamj <ryjamesj@gmail.com>
2024-04-15 15:29:26 -0600

    update

--- src/components/AppStructure/ContactMe/ContactMe.tsx
+++ src/components/AppStructure/ContactMe/ContactMe.tsx
@@ -1,19 +1,30 @@
-import { Box, TextField, Typography, Grid, Button } from "@mui/material";
+import "./ContactMe.scss";
+import React, { useState, useRef } from "react";
+import { Box, TextField, Grid, Button } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import emailjs from "emailjs-com";
-import { useState, useRef } from "react";
-import "./ContactMe.scss";
+
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export default function ContactMe() {
+  const [open, setOpen] = React.useState(false);
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
-  const [subject, setSubject] = useState<string>("");
   const [message, setMessage] = useState<string>("");
-
   const form = useRef<HTMLFormElement>(null);
-  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    setOpen(true);
+
+    e.preventDefault();
     emailjs.sendForm("service_xicbqns", "template_pz87z9c", form.current!, "MdqOAAwT-x0lz6_-q").then(
       () => {
         console.log("SUCCESS!");
@@ -30,15 +41,22 @@ export default function ContactMe() {
       <Grid className="contact-form">
         <form ref={form} onSubmit={sendEmail}>
           <Box className="contact-details">
-            <TextField fullWidth value={name} type="text" onChange={(e) => setName(e.target.value)} label="Name" placeholder="Your name" />
-            <TextField fullWidth value={email} type="email" onChange={(e) => setEmail(e.target.value)} label="Email" placeholder="Your email address" />
-            <TextField fullWidth value={subject} type="text" onChange={(e) => setSubject(e.target.value)} label="Subject" placeholder="Reason for your message" />
+            <TextField fullWidth value={name} type="text" name="from_name" onChange={(e) => setName(e.target.value)} label="Name" placeholder="Your name" />
+            <TextField fullWidth value={email} type="email" name="from_email" onChange={(e) => setEmail(e.target.value)} label="Email" placeholder="Your email address" />
           </Box>
           <Box className="contact-message">
-            <TextField fullWidth value={message} onChange={(e) => setMessage(e.target.value)} label="Message" variant="outlined" multiline rows={4} />
+            <TextField fullWidth value={message} name="message" onChange={(e) => setMessage(e.target.value)} label="Message" placeholder="Your message" multiline rows={4} />
             <Button fullWidth type="submit" endIcon={<SendIcon />} className="contact-me-button">
               Send
             </Button>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">Message Sent!</DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>close</Button>
+              </DialogActions>
+            </Dialog>
           </Box>
         </form>
       </Grid>
